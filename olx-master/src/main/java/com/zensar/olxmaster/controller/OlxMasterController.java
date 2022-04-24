79e400a54f560bda6141a81a3700320abfbe663c
Abhishek <a.sogarwal@zensar.com>
2022-04-24 16:43:20 +0530

    Added layers to olx-master

---
+++ olx-master/src/main/java/com/zensar/olxmaster/controller/OlxMasterController.java
@@ -0,0 +1,36 @@
+package com.zensar.olxmaster.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zensar.olxmaster.entity.Category;
+import com.zensar.olxmaster.entity.Status;
+import com.zensar.olxmaster.service.MasterService;
+
+@RestController
+@RequestMapping("/advertise")
+public class OlxMasterController {
+
+	@Autowired
+	private MasterService masterService;
+	
+	
+	
+	@GetMapping(value="/category",produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+	public List<Category> getAllCategories()
+	{
+		return masterService.getAllCategories();
+	}
+	@GetMapping(value="/status",produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+	public List<Status> getAllStatuses()
+	{
+		return masterService.getAllStatuses();
+	}
+	
+	
+}
